ab92a29a52fa0efe8515f9fcd82a23f62f2b3602
shenjh369 <shenjianhua@163.com>
2018-05-23 07:21:08 +0800

    bind custom event for recipes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,5 @@
 import { Recipe } from './../recipe.model';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,14 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
-    new Recipe('A test recipe', 'this is simple a test', 'http://127.0.0.1:8080/1.png'),
-    new Recipe('A test recipe', 'this is simple a test', 'http://127.0.0.1:8080/2.png'),
-    new Recipe('A test recipe', 'this is simple a test', 'http://127.0.0.1:8080/3.png'),
+    new Recipe('A test recipe1', 'this is simple a test1', 'http://127.0.0.1:8080/1.png'),
+    new Recipe('A test recipe2', 'this is simple a test2', 'http://127.0.0.1:8080/2.png'),
+    new Recipe('A test recipe3', 'this is simple a test3', 'http://127.0.0.1:8080/3.png'),
   ];
   constructor() { }
 
   ngOnInit() {
   }
 
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+    console.log(recipe);
+  }
 }
