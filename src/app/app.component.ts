46473a90daa6098c913e9b87da9ae941ea1f0fa3
shenjh369 <shenjianhua@163.com>
2018-05-23 06:47:29 +0800

    add navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  loadedFeature = 'recipe';
+
   title = 'app';
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
 }
