e02b4f80ab35294f6a440a2ad51b27684a7f1730
shenjh369 <shenjianhua@163.com>
2018-05-24 07:40:07 +0800

    show ingredients of recipe

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,12 +1,22 @@
 import {Recipe} from './recipe.model';
 import {EventEmitter} from '@angular/core';
+import {Ingredient} from '../shared/ingredient.model';
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
   private recipes: Recipe[] = [
-    new Recipe('A test recipe1', 'this is simple a test1', 'http://127.0.0.1:8080/1.png'),
-    new Recipe('A test recipe2', 'this is simple a test2', 'http://127.0.0.1:8080/2.png'),
-    new Recipe('A test recipe3', 'this is simple a test3', 'http://127.0.0.1:8080/3.png'),
+    new Recipe('A test recipe1', 'this is simple a test1', 'http://127.0.0.1:8080/1.png', [
+      new Ingredient('Meat1', 2),
+      new Ingredient('Bread1', 1),
+    ]),
+    new Recipe('A test recipe2', 'this is simple a test2', 'http://127.0.0.1:8080/2.png', [
+      new Ingredient('Meat2', 2),
+      new Ingredient('Bread2', 1),
+    ]),
+    new Recipe('A test recipe3', 'this is simple a test3', 'http://127.0.0.1:8080/3.png', [
+      new Ingredient('Meat3', 2),
+      new Ingredient('Bread3', 1),
+    ]),
   ];
 
   public getRecipes() {
