2b1f204e35dc7c0b9cba6dd937a3147ec1dac945
shenjh369 <shenjianhua@163.com>
2018-05-28 07:40:03 +0800

    add deployment.

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -6,15 +6,15 @@ import {ShoppingListService} from '../shopping-list/shopping-list.service';
 @Injectable()
 export class RecipeService {
   private recipes: Recipe[] = [
-    new Recipe('A test recipe1', 'this is simple a test1', 'http://127.0.0.1:8080/1.png', [
+    new Recipe('A test recipe1', 'this is simple a test1', '/assets/images/1.png', [
       new Ingredient('Meat1', 2),
       new Ingredient('Bread1', 1),
     ]),
-    new Recipe('A test recipe2', 'this is simple a test2', 'http://127.0.0.1:8080/2.png', [
+    new Recipe('A test recipe2', 'this is simple a test2', '/assets/images/2.png', [
       new Ingredient('Meat2', 2),
       new Ingredient('Bread2', 1),
     ]),
-    new Recipe('A test recipe3', 'this is simple a test3', 'http://127.0.0.1:8080/3.png', [
+    new Recipe('A test recipe3', 'this is simple a test3', '/assets/images/3.png', [
       new Ingredient('Meat3', 2),
       new Ingredient('Bread3', 1),
     ]),
